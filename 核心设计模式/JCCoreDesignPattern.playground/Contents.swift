19516c7a25f0d3976711bc680c808fdb34a61bf6
jiachen <624990742@qq.com>
2021-02-07 14:07:44 +0800

    添加KVO使用方法

--- 核心设计模式/JCCoreDesignPattern.playground/Contents.swift
+++ 核心设计模式/JCCoreDesignPattern.playground/Contents.swift
@@ -38,24 +38,70 @@ print(person.name)
 
 ///Mark -  KVO 在swift中的使用
 
-//class KVOClass:NSObject {
-//    dynamic var someValue: String = "123"
-//    var someOtherValue: String = "abc"
-//}
-//
-//class ObserverClass: NSObject {
-//    func observer() {
-//        let kvo = KVOClass()
-//        kvo.addObserver(self, forKeyPath: "someValue", options: .new, context: nil)
-//        kvo.addObserver(self, forKeyPath: "someOtherValue", options: .new, context: nil)
-//        kvo.someValue = "456"
-//        kvo.someOtherValue = "def"
-//        kvo.removeObserver(self, forKeyPath: "someValue")
-//        kvo.removeObserver(self, forKeyPath: "someOtherValue")
-//    }
-//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//        print("\(keyPath!) change to \(change![.newKey] as! String)")
-//    }
-//}
-//ObserverClass().observer()
+/**
+ 用法一：
+ 个人感觉与oc的写法很类似只不过是有一些关键字
+ 
+ 
+ class KVOClass:NSObject {
+    @objc dynamic var currentValue: String = "我可以被监听"
+                  var otherValue: String = "我不能被监听"
+ }
+
+ class ObserverClass: NSObject {
+     
+     override init() {
+         super.init()
+          print("ObserverClass init")
+         self.observer()
+     }
+     
+     let kvo = KVOClass()
+     func observer() {
+         
+         kvo.addObserver(self, forKeyPath: "currentValue", options: .new, context: nil)
+         kvo.addObserver(self, forKeyPath: "otherValue", options: .new, context: nil)
+         kvo.currentValue = "码农晨仔1"
+         kvo.otherValue = "码农晨仔2"
+        
+     }
+     
+     
+     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+         print("\(keyPath!) change to \(change![.newKey] as! String)")
+     }
+     
+     deinit {
+          print("ObserverClass dealloc")
+         kvo.removeObserver(self, forKeyPath: "currentValue")
+         kvo.removeObserver(self, forKeyPath: "otherValue")
+     }
+ }
+  ObserverClass()
+
+ 
+ */
+
+/**
+ 方法二:
+ class KVOTestClass: NSObject {
+     @objc dynamic var name: String?
+     var ob: NSKeyValueObservation?
+     override init() {
+         super.init()
+         name = "你好"
+         ob = self.observe(\KVOTestClass.name, options: [.new, .old]) { (_, change) in
+             print(change)
+         }
+         print("KVOTestClass init")
+     }
+     deinit {
+         print("KVOTestClass dealloc")
+     }
+ }
+
+ var kvoTestObj: KVOTestClass? = KVOTestClass()
+ kvoTestObj?.name = "码农晨仔"
+ kvoTestObj = nil
+ */
 
