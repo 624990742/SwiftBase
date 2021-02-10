34685cbe21480467e643381e8a4bef337aa0bd6b
jiachen <624990742@qq.com>
2021-02-11 01:09:04 +0800

    添加NSoperation的使用

--- 常用核心技术/JCTechnology.playground/Contents.swift
+++ 常用核心技术/JCTechnology.playground/Contents.swift
@@ -185,3 +185,98 @@ group.notify(queue: DispatchQueue.main){
      print("finish")
  }
  */
+
+
+/*
+let operation = BlockOperation {
+            
+    print("任务---\(Thread.current)")
+}
+
+
+operation.addExecutionBlock {
+    
+    print("任务2---\(Thread.current)")
+}
+    
+operation.addExecutionBlock {
+    
+    print("任务3---\(Thread.current)")
+}
+
+
+operation.start()
+
+*/
+
+/*
+ 两种操作方法：
+ 1.把操作对象添加到队列。
+ 2.直接在队列中添加操作任务。
+let queue = OperationQueue()
+let operation1 = BlockOperation {
+    
+    print("任务1---\(Thread.current)")
+}
+   
+let operation2 = BlockOperation {
+    
+    print("任务2---\(Thread.current)")
+}
+    
+//把操作对象添加到队列
+queue.addOperation(operation1)
+queue.addOperation(operation2)
+
+queue.addOperation {
+    print("任务---\(Thread.current)")
+}*/
+
+/*
+let queue = OperationQueue()
+//可以通过修改控制最大并发数来进行并发与串行的测试
+queue.maxConcurrentOperationCount = 1
+// queue.maxConcurrentOperationCount = 2
+let operation1 = BlockOperation {
+    
+    print("任务1---\(Thread.current)")
+}
+   
+let operation2 = BlockOperation {
+    print("任务2---\(Thread.current)")
+}
+queue.addOperation(operation1)
+queue.addOperation(operation2)
+queue.addOperation {
+    print("任务3---\(Thread.current)")
+}
+*/
+
+let queue = OperationQueue()
+        
+let op1 = BlockOperation {
+    
+    print("任务1---\(Thread.current)")
+}
+   
+let op2 = BlockOperation {
+    
+    print("任务2---\(Thread.current)")
+}
+    
+let op3 = BlockOperation {
+    
+    print("任务3---\(Thread.current)")
+}
+//op3依赖于op1，则先完成op1，再完成op3
+op3.addDependency(op1)
+//op1依赖于op2，则先完成op2，再完成op1
+op1.addDependency(op2)
+//最终的依赖关系就是，op2->op1->op3
+ 
+
+queue.addOperation(op1)
+queue.addOperation(op2)
+queue.addOperation(op3)
+
+op1.isReady
